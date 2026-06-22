b71c7b64610c50b9d72fd45ea041201967888fa9
DavidHalla <mr.universepapi@gmail.com>
2026-06-22 12:29:13 -0400

    Created methods for main class and completed tasks

--- practice-fleet.js
+++ practice-fleet.js
@@ -28,8 +28,8 @@ information.
 */
 
 
-class Vehicle {
-    constructor(type, make, model, year, mileage) {
+class vehicle {
+    constructor(type, make, model, year, mileage = 0) {
         this.type = type;
         this.make = make;
         this.model = model;
@@ -38,6 +38,30 @@ class Vehicle {
     }
 
     drive(distance) {
-        
+        return this.mileage += distance;
+    }
+
+    getDetails() {
+        return `These are the details of your vehicle: \n${this.type}.\n${this.make}.\n${this.model}.\n${this.year}.\n${this.mileage}.`;
     }
 }
+
+let newCar = new vehicle("Car", "Toyota", "Supra", "1999", 25620);
+let newBike = new vehicle("Motorcycle", "Harley Davidson", "Speedster", "2010", 5000);
+let newTruck = new vehicle("Truck", "Toyota", "Tacoma", "2022", 99690);
+
+
+console.log(newCar.getDetails());
+console.log(newCar.drive(100));
+
+console.log("\n");
+
+console.log(newBike.getDetails());
+console.log(newBike.drive(200));
+
+console.log("\n");
+
+console.log(newTruck.getDetails());
+console.log(newTruck.drive(500));
+
+console.log("\n");
